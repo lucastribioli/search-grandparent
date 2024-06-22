eaca0b5c807c06c85755d70061f39a41f3748efe
Lucas Tribioli <lucas.tribioli@clickbus.com>
2024-06-22 11:29:56 -0300

    feat: initialization project

---
+++ src/main/java/br/com/lucastribioli/search_grandparent/SearchGrandparentApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucastribioli.search_grandparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchGrandparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchGrandparentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucastribioli/search_grandparent/SearchGrandparentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucastribioli.search_grandparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchGrandparentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
